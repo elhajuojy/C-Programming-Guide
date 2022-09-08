94d40a78025d47f3b1a6278467d2104b15d6d8d4
elhajuojy <souirimehdi311@gmail.com>
2022-09-08 19:13:12 +0100

    Exercice C 📜 💻

---
+++ Exercices/Exercice4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    float note1,note2,note3,moynne;
+    printf("please entre your note n1");
+    scanf("%f",&note1);
+    printf("please entre your note n1");
+    scanf("%f",&note2);
+    printf("please entre your note n1");
+    scanf("%f",&note3);
+
+    moynne = (note1+note2+note3)/3;
+    printf("moynne : %f",moynne);
+
+
+    return 0;
+
+}
