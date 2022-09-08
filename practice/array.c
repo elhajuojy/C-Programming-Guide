1cdfbb46ee9642f0debc73471d7cfd4ed2331901
elhajuojy <souirimehdi311@gmail.com>
2022-09-08 19:13:42 +0100

    change dir 🌌

--- practice/array.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-void main(){
-    // printf("arr");
-    // dataType arrayName[arraySize];
-    // int mark[5] = {19, 10, 8, 17, 9};
-    // int mark[] = {19, 10, 8, 17, 9};
-}
