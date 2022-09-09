08e7e1b59651f602a572d567433535cadbacd522
elhajuojy <souirimehdi311@gmail.com>
2022-09-09 11:07:00 +0100

    print tirangle 🫠🫠

---
+++ shapes/triangle.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ 
+int main() {
+    int i, j, numberOfRows, star ;
+    printf("Please entre the number :");
+    scanf("%d", &numberOfRows);
+
+        for (i = 0; i < numberOfRows; i++) {
+
+        for (j = 0; j < (numberOfRows - i ); j++) {
+            printf(" ");
+        }
+        
+        star=0;
+        while (star <= i+1)
+        {
+            printf("*");
+            star++;
+        };
+
+        star = 0;
+        printf("\n");
+    }
+   
+   
+    return 0;
+}
